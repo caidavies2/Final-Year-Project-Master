cec0ae2ebd6c4ecbffab8ac720993a2bb272c77c
caidavies <hello@caidavies.me>
2014-05-16 15:33:38 +0100

    Updated Tiny URL
    Read to put in main ‘add’ section (functions assigned to variables)

--- database-connect/tiny-url.php
+++ database-connect/tiny-url.php
@@ -6,32 +6,39 @@ $extension = substr($url, ($length-3), $length);
 $data=file_get_contents($url); 
 $meta = get_meta_tags($url);
 
-if(checkExtension($extension) == true)
-{
-  echo "<br>";
-}
-else
-{
-  getTitle($data, $matches);
-  echo "<br>";
-}
-getDescription($data, $matches);
-tinyUrl($url);
+$description;
+$compressedURL;
+$title = getTitle($data, $matches, $extension);
+$description = getDescription($data, $matches);
+$tinyURL = tinyUrl($url);
 
-function getTitle($data, $matches)
+echo $title;
+echo $description;
+echo $tinyURL;
+
+function getTitle($data, $matches, $extension)
 {
-  if(preg_match("/<title>(.+)<\/title>/i", $data, $matches))
+  if(checkExtension($extension) == true)
   {
-    echo "$matches[1]";
+    return checkExtension($extension);
+  }
+  // else
+  // {
+  //   getTitle($data, $matches);
+  // }
+
+  else if(preg_match("/<title>(.+)<\/title>/i", $data, $matches))
+  {
+    return "$matches[1]";
   }
   else if(preg_match("/<h1>(.+)<\/h1>/i", $data, $matches))
   {
-    echo "$matches[1]";
+    return "$matches[1]";
   }
 
   else
   {
-    echo $meta['title'];
+    return $meta['title'];
     // Put in code to 'link 3 etc' if no title or h1 tags.
   }
 
@@ -41,15 +48,15 @@ function getDescription($data, $matches)
 {
   if(preg_match("/<p>(.+)<\/p>/i", $data, $matches))
   {
-    echo "$matches[1]";
+    return "$matches[1]";
   }
   else if(preg_match("/<h2>(.+)<\/h2>/i", $data, $matches))
   {
-    echo "$matches[1]";
+    return "$matches[1]";
   }
   else
   {
-    $meta['description'];
+   return $meta['description'];
   }
 }
 
@@ -66,8 +73,7 @@ function checkExtension($extension)
       || $extension == "svg"
       || $extension == "raw") 
   {
-    echo "Image:";
-    return true;
+    return "Image:";
   }
   
   else if (
@@ -79,8 +85,7 @@ function checkExtension($extension)
     || $extension == "m4a"
     )
   {
-    echo "Sound File:";
-    return true;
+    return "Sound File:";
   }
   else {return false;}
 }
@@ -99,7 +104,8 @@ function tinyUrl($lurl)
   curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
   $result = curl_exec($curl);
   curl_close($curl);
-  echo $result;
-  echo "<br>";
+  return $result;
 }
+
+
 ?>
