14c0342ba80105985e46c843bf797480b4e428f2
caidavies <hello@caidavies.me>
2014-05-16 14:42:12 +0100

    Updated Tiny URL file

--- database-connect/tiny-url.php
+++ database-connect/tiny-url.php
@@ -1,108 +1,89 @@
 <?php
 
-$url = "http://cl.ly/image/1p0S053T121L/Screen%20Shot%202014-04-30%20at%206.48.31%20PM.png";
+$url = "http://en.wikipedia.org/wiki/Audio_file_format";
 $length = strlen($url);
 $extension = substr($url, ($length-3), $length);
-echo $extension;
+$data=file_get_contents($url); 
+$meta = get_meta_tags($url);
 
-echo pageTitle($url);
-echo "<br>";
-echo pageDescription($url);
-echo "<br>";
-echo tinyUrl($url);
-
-
-function metaData($link, $type)
+if(checkExtension($extension) == true)
 {
-
-  //       // create curl resource
-  // $ch = curl_init();
-
-  //       // set url
-  // curl_setopt($ch, CURLOPT_URL, $url);
-
-  //       //return the transfer as a string
-  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-
-  //       // Alternatively, lie, and pretend to be a browser
-  // curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)');
-
-  //       // $output contains the output string
-  // $output = curl_exec($ch);
-
-  //       // close curl resource to free up system resources
-  // curl_close($ch); 
-  if($type == 'title')
-  {
-    $firstParagraphTag = strpos($output, "<div");
-    $nextParagraphTag = strpos($output, "</div>");
-    echo substr($output, $firstParagraphTag, $nextParagraphTag);
-  }
-  else if($type == 'description')
-  {
-        $firstParagraphTag = strpos($output, "<div");
-        $nextParagraphTag = strpos($output, "</div>");
-        echo substr($output, $firstParagraphTag, $nextParagraphTag);
-  }
-
+  echo "<br>";
 }
-
-function pageTitle($page_url)
-{
- 
- $read_page=file_get_contents($page_url);
- preg_match("/<title.*?>[\n\r\s]*(.*)[\n\r\s]*<\/title>/", $read_page, $page_title);
-
-if(pageExtension($url))
-{
-  pageExtension($extension);
-}
- else if (isset($page_title[1]))
- {
-  if ($page_title[1] == '')
-  {
-    return $page_url;
-  }
-  $page_title = $page_title[1];
-  return trim($page_title);
-}
-
 else
 {
-    metaData($url, 'title');
+  getTitle($data, $matches);
+  echo "<br>";
 }
-}
+getDescription($data, $matches);
+tinyUrl($url);
 
-function pageExtension($extension)
+function getTitle($data, $matches)
 {
-
-  if($extension == 'jpg' || $extension == 'png')
+  if(preg_match("/<title>(.+)<\/title>/i", $data, $matches))
   {
-    echo 'Image:';
+    echo "$matches[1]";
   }
-  return true;
+  else if(preg_match("/<h1>(.+)<\/h1>/i", $data, $matches))
+  {
+    echo "$matches[1]";
+  }
+
+  else
+  {
+    echo $meta['title'];
+    // Put in code to 'link 3 etc' if no title or h1 tags.
+  }
+
 }
 
-function pageDescription($url)
+function getDescription($data, $matches)
 {
-  $tags = get_meta_tags($url);
-
-  $description = $tags['description'];
-
-  
-  if(!description)
+  if(preg_match("/<p>(.+)<\/p>/i", $data, $matches))
   {
-      metaData($url, 'description');
+    echo "$matches[1]";
+  }
+  else if(preg_match("/<h2>(.+)<\/h2>/i", $data, $matches))
+  {
+    echo "$matches[1]";
   }
   else
   {
-    return substr($description, 0, strpos($description, ' ', 50));
+    $meta['description'];
   }
-
 }
 
+function checkExtension($extension)
+{
+  if
+    (
+      $extension == "jpg"
+      || $extension == "jpeg"
+      || $extension == "png"
+      || $extension == "gif"
+      || $extension == "bmp"
+      || $extension == "tiff"
+      || $extension == "svg"
+      || $extension == "raw") 
+  {
+    echo "Image:";
+    return true;
+  }
+  
+  else if (
+    $extension == "mp3" 
+    || $extension == "wav" 
+    || $extension == "3gp" 
+    || $extension == "ogg"
+    || $extension == "aiff"
+    || $extension == "m4a"
+    )
+  {
+    echo "Sound File:";
+    return true;
+  }
+  else {return false;}
+}
 function tinyUrl($lurl)
 {
 
@@ -121,5 +102,4 @@ function tinyUrl($lurl)
   echo $result;
   echo "<br>";
 }
-
 ?>
